0f74f2ceb048a558f4ea467e0a51388e921e4ff5
SA-Root <a124000@outlook.com>
2021-04-14 18:58:50 +0800

    Update PDU Frame

--- RGBN/src/main/java/com/vinewood/PDUFrame.java
+++ RGBN/src/main/java/com/vinewood/PDUFrame.java
@@ -14,7 +14,6 @@ public class PDUFrame {
     public short AckNo;
     public byte[] Data;
     public short Checksum;
-    private static final byte Flag = 0b01110111;
 
     /**
      * Serialize a PDU frame. Automatically calculates checksum.
@@ -28,21 +27,14 @@ public class PDUFrame {
     public static byte[] SerializeFrame(byte ft, short sn, short an, byte[] dat) {
         byte[] buffer = new byte[9 + dat.length * 2];
         int pos = 0;
-        buffer[pos++] = Flag;// LFlag
         buffer[pos++] = ft;// FrameType
         pos = RGBN_Utils.ToByteArray(sn, buffer, pos);// SeqNo
         pos = RGBN_Utils.ToByteArray(an, buffer, pos);// AckNo
         // Data
         for (byte b : dat) {
-            if (b == Flag) {
-                buffer[pos++] = Flag;
-                buffer[pos++] = b;
-            } else {
-                buffer[pos++] = b;
-            }
+            buffer[pos++] = b;
         }
         pos = RGBN_Utils.ToByteArray((short) VW_CRC16.CRC16CCITT(dat), buffer, pos);// Checksum
-        buffer[pos++] = Flag;// RFlag
         byte[] ret = Arrays.copyOfRange(buffer, 0, pos);
         return ret;
     }
@@ -57,11 +49,6 @@ public class PDUFrame {
     public static PDUFrame DeserializeFrame(byte[] stream) throws ChecksumMismatchException {
         PDUFrame ret = new PDUFrame();
         int pos = 0;
-        // LFlag
-        if (stream[pos] != Flag) {
-            throw new ChecksumMismatchException();
-        }
-        ++pos;
         // FrameType
         ret.FrameType = stream[pos++];
         // SeqNo
@@ -71,25 +58,13 @@ public class PDUFrame {
         ret.AckNo = RGBN_Utils.ShortFromByteArray(stream, pos);
         pos += 2;
         // Data
-        int dataEnd = stream.length - 4;
-        byte[] buffer = new byte[dataEnd - pos + 1];
-        int dataPos = 0;
-        while (pos <= dataEnd) {
-            if (stream[pos] == Flag) {
-                ++pos;
-            }
-            buffer[dataPos++] = stream[pos++];
-        }
-        ret.Data = Arrays.copyOf(buffer, buffer.length);
+        int dataEnd = stream.length - 2;
+        ret.Data = Arrays.copyOfRange(stream, pos, dataEnd);
         // Checksum
         short ck = (short) VW_CRC16.CRC16CCITT(ret.Data);
         short crc = RGBN_Utils.ShortFromByteArray(stream, pos);
         pos += 2;
         if (ck != crc) {
-            throw new ChecksumMismatchException();
-        }
-        // RFlag
-        if (stream[pos] != Flag) {
             throw new ChecksumMismatchException();
         }
         return ret;
